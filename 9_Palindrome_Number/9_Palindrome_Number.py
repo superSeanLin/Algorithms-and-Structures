83774eb3ea25829f673073cbf68d2a2b303289f0
superSeanLin <linxun034@gmail.com>
2019-01-15 16:14:04 -0600

    Create 9_Palindrome_Number.py

---
+++ 9_Palindrome_Number/9_Palindrome_Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        return str(x) == str(x)[::-1]
+        
