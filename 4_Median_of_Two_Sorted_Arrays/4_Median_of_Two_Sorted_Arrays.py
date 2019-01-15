96b86a3222c31f86be47488b115c7548849dc251
superSeanLin <linxun034@gmail.com>
2019-01-15 11:00:40 -0600

    Update 4_Median_of_Two_Sorted_Arrays.py

--- 4_Median_of_Two_Sorted_Arrays/4_Median_of_Two_Sorted_Arrays.py
+++ 4_Median_of_Two_Sorted_Arrays/4_Median_of_Two_Sorted_Arrays.py
@@ -16,20 +16,18 @@ class Solution:
             ans = self.quickSelect(nums1 + nums2, length // 2)
         return ans
     
-    def quickSelect(self, nums, index):
+    def quickSelect(self, nums, index):  # in-place partitionn
         length = len(nums)
+        nums[length//2], nums[length-1] = nums[length-1], nums[length//2]  # swap median with the last element
         median = nums[length-1]
-        #print(nums)
-        i = 0
-        j = length - 1
-        while i < j:  # until pointers meet
-            while nums[i] <= median and i < j:  # stop when greater than or equal to median
+        i = 0 # sorted index
+        j = 0 # j >= i
+        while j < length-1:  # check through the whole list
+            if nums[j] < median:
+                nums[i], nums[j] = nums[j], nums[i]  # swap nums[i] and nums[j], since nums[i] >= median, nums[j] < median
                 i += 1
-            nums[i], nums[j] = nums[j], nums[i]
-            while nums[j] >= median and i < j:  # stop when less or equal than median
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
-        #print(nums, median, i, index)
+            j += 1
+        nums[length-1], nums[i] = nums[i], nums[length-1]  # i is the final index of median
         res = 0
         if index < i:  # recurse in smaller list
             res = self.quickSelect(nums[:i], index)
