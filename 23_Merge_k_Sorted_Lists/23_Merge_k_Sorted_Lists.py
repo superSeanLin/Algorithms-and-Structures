4f683965b50298bb0085fb570ebabde9e7c3f6db
superSeanLin <linxun034@gmail.com>
2019-01-17 20:40:56 -0600

    Create 23_Merge_k_Sorted_Lists.py

---
+++ 23_Merge_k_Sorted_Lists/23_Merge_k_Sorted_Lists.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    ## Need better solution
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        minimum = 1000
+        p = None
+        for l in lists:  # find min of all lists
+            if l and l.val < minimum:
+                p = l
+                minimum = l.val
+        if p:  # not all None
+            q = p
+            lists[lists.index(p)] = p.next  # change original lists
+            q.next = self.mergeKLists(lists)
+            return q
+        else:  # all None
+            return None
