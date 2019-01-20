ee5fb3e63365fd7ae84ed77d940bc055523ad843
superSeanLin <linxun034@gmail.com>
2019-01-19 19:33:55 -0600

    Create 29_Divide_Two_Integers.py

---
+++ 29_Divide_Two_Integers/29_Divide_Two_Integers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        negative = 1
+        if dividend * divisor < 0:  # different sign
+            negative = -1
+        # make same sigb
+        divisor = abs(divisor)
+        dividend = abs(dividend)
+        quotient = 0
+        while dividend >= divisor:
+            temp, count = divisor, 1
+            while dividend >= temp:  # multiply-like; Note: also for exp
+                dividend -= temp
+                temp += temp
+                quotient += count
+                count += count
+        return min(max(negative * quotient, -2**31), 2**31-1)
