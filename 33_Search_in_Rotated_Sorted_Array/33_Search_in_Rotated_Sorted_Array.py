da66ac7a5e1da5a61cda2c9a76d9060f9014d90f
superSeanLin <linxun034@gmail.com>
2019-02-02 22:48:44 -0600

    Update 33_Search_in_Rotated_Sorted_Array.py

--- 33_Search_in_Rotated_Sorted_Array/33_Search_in_Rotated_Sorted_Array.py
+++ 33_Search_in_Rotated_Sorted_Array/33_Search_in_Rotated_Sorted_Array.py
@@ -10,15 +10,12 @@ class Solution:
         if len(nums) <= 0:
             return -1
         low, high = 0, len(nums)-1
-        middle = (low + high) // 2
-        if nums[middle] != target:
-            left = self.search(nums[low:middle], target)
-            right = self.search(nums[middle+1:high+1], target)
-            if left != -1:
-                return low + left
-            elif right != -1:
-                return middle+1+right
-            else:  # no find
-                return -1
-        else:
-            return middle
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+            elif nums[0] <= target < nums[mid] or target < nums[mid] < nums[0] or nums[mid] < nums[0] <= target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
