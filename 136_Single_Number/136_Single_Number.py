83a4c813cc8a4639c71dc082186f2839827c6836
superSeanLin <linxun034@gmail.com>
2019-02-12 19:58:35 -0600

    Update 136_Single_Number.py

--- 136_Single_Number/136_Single_Number.py
+++ 136_Single_Number/136_Single_Number.py
@@ -1,3 +1,7 @@
 class Solution:
     def singleNumber(self, nums: 'List[int]') -> 'int':
-        return 2*sum(set(nums)) - sum(nums)
+        ## a XOR a = 0; a XOR 0 = a
+        res = 0
+        for i in nums:
+            res ^= i
+        return res
