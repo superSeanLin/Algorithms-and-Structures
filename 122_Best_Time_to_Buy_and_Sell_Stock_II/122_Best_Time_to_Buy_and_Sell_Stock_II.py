32ccb897b2900d05045c9f24502469ec405afb42
superSeanLin <linxun034@gmail.com>
2019-02-20 16:29:47 -0600

    Update 122_Best_Time_to_Buy_and_Sell_Stock_II.py

--- 122_Best_Time_to_Buy_and_Sell_Stock_II/122_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II/122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -4,16 +4,24 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if not prices:
-            return 0
-        buy, sell = prices[0], prices[0]
+        ## method 1: find peak
+        # if not prices:
+        #     return 0
+        # buy, sell = prices[0], prices[0]
+        # sum = 0
+        # for i in range(1, len(prices)):
+        #     if prices[i] > sell:
+        #         sell = prices[i]
+        #     if prices[i] < sell or i == len(prices)-1:  # already sell or last one    
+        #         diff = sell - buy
+        #         sum += diff
+        #         buy, sell = prices[i], prices[i]
+        # return sum
+        
+        ## method 2: accumulate all increase
         sum = 0
-        for i in range(1, len(prices)):
-            if prices[i] > sell:
-                sell = prices[i]
-            if prices[i] < sell or i == len(prices)-1:  # already sell or last one    
-                diff = sell - buy
-                sum += diff
-                buy, sell = prices[i], prices[i]
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                sum += prices[i+1] - prices[i]
         return sum
                 
