e6c7f907b44b97138fe0d12ef24b8662bd2a5684
superSeanLin <linxun034@gmail.com>
2019-03-29 14:51:40 -0500

    Create 287_Find_the_Duplicate_Number.py

---
+++ 287_Find_the_Duplicate_Number/287_Find_the_Duplicate_Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        ## build graph and find entrance of the cycle
+        # we consider nums list as [index node: next node], if there is duplicates, then must two different index nodes -> same nodes; we use fast and slow pointer to detect the cycle
+        slow = nums[0]
+        fast = nums[0]
+        while True:  # unitil meet
+            slow = nums[slow]
+            fast = nums[nums[fast]]  # fast is twice quicker than slow
+            if slow == fast:
+                break
+        # reset fast to start
+        fast = nums[0]
+        while fast != slow:
+            fast = nums[fast]
+            slow = nums[slow]
+        return fast
