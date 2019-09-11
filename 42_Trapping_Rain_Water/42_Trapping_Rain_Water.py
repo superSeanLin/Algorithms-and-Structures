303bba246cc9f2e0c0c1906a5176a25bfbc452d7
superSeanLin <linxun034@gmail.com>
2019-09-11 00:21:00 -0700

    Update 42_Trapping_Rain_Water.py

--- 42_Trapping_Rain_Water/42_Trapping_Rain_Water.py
+++ 42_Trapping_Rain_Water/42_Trapping_Rain_Water.py
@@ -2,8 +2,24 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         ## intuition: find left max and right max to calculate
         
+        ## use DP; find maximum height on the left and maximum height on the right for each index
+        if not height:
+            return 0
+        n = len(height)
+        left, right = [0]*n, [0]*n
+        left[0] = height[0]
+        for i in range(1, n):
+            left[i] = max(height[i], left[i-1])  # when unbounded, like index 0, it will use itself
+        right[-1] = height[-1]
+        for i in range(n-2, -1, -1):
+            right[i] = max(height[i], right[i+1])
+        res = 0
+        for i in range(n):
+            res += (min(left[i], right[i]) - height[i])
+        return res
+        
         ## use stack 
-        # index = []  # store height index
+        # index = []  # store height index; descending stack
         # res = 0
         # for i in range(len(height)):
         #     while len(index) > 0 and height[i] > height[index[-1]]:  # if current > prev, could trap water
@@ -16,21 +32,21 @@ class Solution:
         # return res
     
         ## two pointer
-        left, right = 0, len(height)-1
-        leftMax, rightMax = 0, 0
-        res = 0
-        while left < right:
-            if height[left] < height[right]:  # depends on smaller bar
-                if height[left] > leftMax:
-                    leftMax = height[left]
-                else:
-                    res += leftMax - height[left]
-                left += 1
-            else:  # depends on smaller bar
-                if height[right] > rightMax:
-                    rightMax = height[right]
-                else:
-                    res += rightMax - height[right]
-                right -= 1
-        return res
+#         left, right = 0, len(height)-1
+#         leftMax, rightMax = 0, 0
+#         res = 0
+#         while left < right:
+#             if height[left] < height[right]:  # depends on smaller bar; until find a valid range
+#                 if height[left] > leftMax:
+#                     leftMax = height[left]
+#                 else:
+#                     res += leftMax - height[left]
+#                 left += 1
+#             else:  # depends on smaller bar
+#                 if height[right] > rightMax:
+#                     rightMax = height[right]
+#                 else:
+#                     res += rightMax - height[right]
+#                 right -= 1
+#         return res
                 
