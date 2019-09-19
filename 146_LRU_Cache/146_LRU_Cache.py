b052340e982c6c73408b2dfef40f15378d874f57
superSeanLin <linxun034@gmail.com>
2019-09-18 23:07:37 -0700

    Update 146_LRU_Cache.py

--- 146_LRU_Cache/146_LRU_Cache.py
+++ 146_LRU_Cache/146_LRU_Cache.py
@@ -1,30 +1,60 @@
-class LRUCache:
-
-    def __init__(self, capacity: 'int'):
-        self.cache = {}
-        self.capacity = capacity
-        self.queue = []
-
-    def get(self, key: 'int') -> 'int':
-        if key in self.cache and self.cache[key] != -1:
-            self.queue = self.updateQ(self.queue, key)  # valid update
-            return self.cache[key]
-        else:
-            return -1
-
-    def put(self, key: 'int', value: 'int') -> 'None':
-        if not key in self.queue and len(self.queue)+1 > self.capacity:  # need eviction
-            evict = self.queue.pop(0)
-            self.cache[evict] = -1
-        self.queue = self.updateQ(self.queue, key)  # make sure queue is the right length and concise
-        self.cache[key] = value
+class DoubleLinkedNode:
+    def __init__(self, key, value):
+        self.pre = None
+        self.nxt = None
+        self.val = value
+        self.key = key 
     
-    def updateQ(self, queue, key):  # same key should be updated
-        queue = [e for e in queue if e != key]
-        queue.append(key)
-        while len(queue) > self.capacity:
-            queue.pop(0)
-        return queue
+class LRUCache:
+    # use hashmap with double linked list to achieve O(1); the important property of double linked list is that we can delete a node without knowing other node
+    # Note: also we can not delete node from cache, instead, we set value to -1
+    def __init__(self, capacity: int):
+        self.cache = {}  # key : node
+        self.count = 0
+        self.capacity = capacity
+        self.head, self.tail = DoubleLinkedNode(-1, -1), DoubleLinkedNode(-1, -1)  # set boundery
+        self.head.nxt = self.tail
+        self.tail.pre = self.head
+    
+    def remove(self, node):
+        pre = node.pre
+        nxt = node.nxt
+        pre.nxt = nxt
+        nxt.pre = pre
+    
+    def addToHead(self, node):
+        temp = self.head.nxt
+        self.head.nxt = node
+        node.pre = self.head
+        node.nxt = temp
+        temp.pre = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.remove(node)
+        self.addToHead(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:  # update
+            node = self.cache[key]
+            self.remove(node)
+            self.addToHead(node)
+            node.val = value
+        else:
+            node = DoubleLinkedNode(key, value)
+            self.addToHead(node)
+            self.count += 1
+            if self.count > self.capacity:
+                temp = self.tail.pre
+                self.remove(temp)
+                del self.cache[temp.key]  # delete node in hashmap is O(1)
+                self.count -= 1
+            self.cache[key] = node
+            
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
