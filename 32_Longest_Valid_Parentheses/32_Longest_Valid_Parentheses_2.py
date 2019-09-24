33656daa46eec3c4727b35c233e92309c7650f89
superSeanLin <linxun034@gmail.com>
2019-09-23 17:58:36 -0700

    Create  32_Longest_Valid_Parentheses_2.py

---
+++ 32_Longest_Valid_Parentheses/32_Longest_Valid_Parentheses_2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        # use left/right counter
+        left, right = 0, 0
+        n = len(s)
+        maxi = 0
+        for i in range(n):  # from left to right
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                maxi = max(maxi, left*2)
+            if right > left:  # not valid anymore
+                left, right = 0, 0
+        left, right = 0, 0
+        for i in range(n-1, -1, -1):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                maxi = max(maxi, left*2)
+            if left > right:
+                left, right = 0, 0
+        return maxi
